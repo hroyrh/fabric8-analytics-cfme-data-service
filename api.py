de8a9a2d6315d3bf5a94fd977ae78f09531e1fba
Tomáš Hrčka <thrcka@redhat.com>
2017-07-13 14:21:06 +0200

    Add json data from logs

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
-from flask import Flask, url_for
+import os
+from flask import Flask, render_template_string, url_for, json
 app = Flask(__name__)
 
 @app.route('/')
 def api_root():
     return 'Welcome'
+
 
 @app.route('/workers')
 def api_articles():
@@ -12,6 +14,14 @@ def api_articles():
 @app.route('/workers/<workerid>')
 def api_article(workerid):
     return 'You are reading ' + workerid
+
+@app.route('/raw_json')
+def showjson():
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "data", "workers_output.json")
+    data = json.load(open(json_url))
+    
+    return render_template_string('{{data}}', data=data)
 
 if __name__ == '__main__':
     app.run()
